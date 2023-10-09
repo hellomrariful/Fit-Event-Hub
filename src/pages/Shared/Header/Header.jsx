10dcfbca406b60c1a33de2aee7186a76b07ca8aa
Ariful Islam <hello.mrariful@gmail.com>
2023-10-09 23:28:29 +0600

    Solved some bugs & Web is now fully ready

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
               </span>
               <span>
                 <Link to="/gallery">
-                  <button className="md:px-8 px-4 py-4 mt-4 text-xl font-medium text-white capitalize transition-colors duration-300 transform outline lg:w-auto hover:bg-[#F0F0F0] focus:outline-none focus:bg-blue-500">
+                  <button className="md:px-8 px-4 py-4 mt-4 text-xl font-medium hover:text-black text-white capitalize transition-colors duration-300 transform outline lg:w-auto hover:bg-[#F0F0F0] focus:outline-none focus:bg-blue-500">
                     Gallery
                   </button>
                 </Link>
